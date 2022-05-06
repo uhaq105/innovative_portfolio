822a9e66230b2bd08c601f630f092a90f084bdf6
Usman Haq <haqusman010@gmail.com>
2022-05-06 18:57:41 +0100

    list tuples

---
+++ list-tuples.py
@@ -0,0 +1,30 @@
+#even_nums = [2, 4, 6, 8, 10]
+
+#odd_nums = [1, 3, 5, 7, 9]
+#even_nums.append(12)
+
+#even_nums.insert(5, 0)
+
+#for i in even_nums:
+#    print(i)
+
+list_of_fruit = [
+    'apples',
+    'bananas',
+    'oranges',
+    'pears',
+    'grapes',
+    'pineapple',
+    'star-fruit',
+    'mango',
+    'cherries',
+    'black-berries'
+]
+
+nums = [0, 1, 2 ,3 ,4, 5, 6, 7, 8, 9, 10]
+
+num = 0
+
+
+while some_variable:
+    print()
